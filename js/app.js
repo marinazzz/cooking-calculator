0581ba38658792dd57f4e817a0974d80d700f19d
Marina Zujic <zujicmarina@gmail.com>
2020-05-09 21:29:55 +0200

    Finished style full responsive

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let form = document.getElementById ('form');
+let recipeName = document.getElementById ('recipe-name');
+let servingsAmount = document.getElementById ('servings-amount');
+let needsToserve = document.getElementById ('needs-to-serve');
+
+
