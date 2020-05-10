8c92037534ccd8e51e4f53d832d4c94e7971142b
Milos Radibratovic <radibratovic.milos@gmail.com>
2020-05-10 14:48:12 +0200

    Added input styles to style.css, added comments in index.html as guidelines

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-let form = document.getElementById ('form');
-let recipeName = document.getElementById ('recipe-name');
-let servingsAmount = document.getElementById ('servings-amount');
-let needsToserve = document.getElementById ('needs-to-serve');
-
-
+let form = document.getElementById('form');
+let recipeName = document.getElementById('recipe-name');
+let servingsAmount = document.getElementById('servings-amount');
+let needsToserve = document.getElementById('needs-to-serve');
