0613dd3e750a050d0b4712a4063fbfd9adda45c3
Marina Zujic <zujicmarina@gmail.com>
2020-05-16 20:51:18 +0200

    Form validation,prevent default

--- js/app.js
+++ js/app.js
@@ -4,14 +4,49 @@ document.getElementById('form').addEventListener("submit", function(event){
     event.preventDefault();
 });
 */
+/*
+let form = document.getElementById('form');
+form.addEventListener ('submit', (event) => {
+    event.preventDefault();
+    let recipeName = document.forms["calculator"]["originalEnter"];
+    let message = document.querySelector('.required');
+    let alertMessage = '';
+    if (recipeName.value === '' || recipeName.value == null) {
+        alertMessage = 'Recipe name is required.';
+        message.innerHTML = alertMessage;
+        return false;
+    }
+});*/
+
+
 
 let form = document.getElementById('form');
-form.addEventListener ('submit', function(event){
+form.addEventListener ('submit', (event) => {
     event.preventDefault();
+    let recipeName = document.getElementById('recipe');
+    let servings = document.getElementById('originalServings');
+    let needServings = document.getElementById('needServings');
+    let message = document.createElement('p');
+    message.className= 'error';
+
+    if (recipeName.value == "" || recipeName.value.trim()== "") {
+        message.innerText = '*Recipe name is required.';
+        document.querySelector('.input-container__input-name').append(message);
+        return false;
+    }
+
+
+    if (servings.value == "" || servings.value.trim()== "") {
+        message.innerText = '*Original recipe servings is required.';
+        document.querySelector('.input-container__input-original').append(message);
+        return false;
+    }
+
+    if (needServings.value == "" || needServings.value.trim()== "") {
+        message.innerText = '*Recipe servings is required.';
+        document.querySelector('.input-container__input-need').append(message);
+        return false;
+    }
+    return true;
 });
-
-
-//message
-
-let recipeName = document.queryCommandIndeterm('recipe-name');
 
